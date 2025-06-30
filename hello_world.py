37027d4d5ec310c25f6921ea831d81f4134cef20
VoltronDiscovery <135940101+VoltronDiscovery@users.noreply.github.com>
2025-06-30 20:41:18 +1200

    Update

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+
+def main():
+    print("Hello World!")
+    print("AI")
+    andre(2)
+
+def andre(a):
+    
+    print(a)
+    
+
+# Program Entrance Check
+if __name__ == '__main__':
+    main()
