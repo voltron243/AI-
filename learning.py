6d05e8732309774af8cb0c529e56e4bd76bfef91
VoltronDiscovery <135940101+VoltronDiscovery@users.noreply.github.com>
2025-07-02 20:27:28 +1200

    Update 1.03

---
+++ learning.py
@@ -0,0 +1,51 @@
+# # Operators 
+# print(10 + 6)
+# print(14 * 2)
+# print(12 ** 2)
+# print(12 / 6)
+# a, b = 12, 5
+
+# a + b 
+# print(a + b)
+
+# name = ("Andre")
+# print(name) 
+
+# # Python for fruits 
+# fruits = ["apple", "banana", "strawberry"]
+# for x in fruits:
+#     print(x)
+
+# # Boolen 
+# a = 10
+# b = 20
+
+# print(a > b)
+
+# # function 
+# def my_function():
+#     print("Andre")
+
+# my_function()
+
+# # type 
+# print(type(x))
+
+# if statments 
+a = 10
+b = 10
+
+# if b > a:
+#     print("b is greater than a")
+
+# Only use elif if the previous one is not true
+if b < a:
+    print("b is smaller than a")
+elif b == a:
+    print("b and a are the same")
+    
+ 
+    
+
+
+
